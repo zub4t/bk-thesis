cbdade5dd5bb0aed18bbaf1eff69dbbc2cddd079
marco primo <up201800388@fc.up.pt>
2023-05-08 14:51:41 +0100

    view 6tag 4per4

--- gui/path_of_a_constant_subgroup.py
+++ gui/path_of_a_constant_subgroup.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from PIL import Image, ImageTk
 from collections import Counter
 import sys
 import json
@@ -14,23 +16,22 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from sklearn.cluster import DBSCAN
 import numpy as np
-from GradientDescent import GradientDescent
+from GradientDescentFixedZ import GradientDescent
 from Measurement import Measurement
 colors=None
 measurements_dict, mobile_location_dict = Util.read_json_file(
-    "../JSON/file.json", "802.11mc"
+    "../JSON/file.json", "uwb"
 )
-print(measurements_dict)
 with open("../JSON/AP_location.json", "r") as f:
     ap_location_raw = json.load(f)
 ap_locations = {}
 for e in ap_location_raw:
-    ap_locations[e["BSSID"]] = {"x": e["X"], "y": e["Y"], "z": e["Z"]}
+    ap_locations[e["ID"]] = {"x": e["X"], "y": e["Y"], "z": e["Z"]}
 # create the main window
 gradient_descent = GradientDescent(
     learning_rate=0.01, max_iterations=1000, tolerance=1e-5
 )
-bias = lambda x: x / 1.16 - 0.63
+bias = lambda x: x #/ 1.16 - 0.63
 
 
 def process(subgroup_size, exp):
@@ -38,81 +39,170 @@ def process(subgroup_size, exp):
     filtered_dict = {
         k: [obj for obj in v if obj.exp == exp] for k, v in measurements_dict.items() if any(obj.exp == exp for obj in v)
     }
-    sampled_list = random.sample(filtered_dict.keys(), 6)
+
+
+
+    # new_data = {}
+    # delta_t = 5000  # the maximum time difference to consider
+    # for key, measurements in filtered_dict.items():
+    #     new_measurements = []
+    #     current_measurements = []
+    #     current_timestamp = None
+    #     for measurement in measurements:
+    #         if current_timestamp is None:
+    #             current_timestamp = measurement.timestamp
+    #             current_measurements.append(measurement)
+    #         elif measurement.timestamp - current_timestamp <= delta_t:
+    #             current_measurements.append(measurement)
+    #         else:
+    #             # calculate the average distance for the current timestamp
+    #             distances = [m.distance for m in current_measurements]
+    #             avg_distance = sum(distances) / len(distances)
+    #             current_measurements[0].distance = avg_distance
+    #             new_measurement = current_measurements[0]
+    #             new_measurements.append(new_measurement)
+    #
+    #             # reset the current measurements list and timestamp
+    #             current_measurements = [measurement]
+    #             current_timestamp = measurement.timestamp
+    #
+    #     # add the last average measurement
+    #     if current_measurements:
+    #         distances = [m.distance for m in current_measurements]
+    #         avg_distance = sum(distances) / len(distances)
+    #         current_measurements[0].distance = avg_distance
+    #         new_measurement = current_measurements[0]
+    #         new_measurements.append(new_measurement)
+    #
+    #     # add the new measurements to the new dictionary
+    #     new_data[key] = new_measurements
+    # filtered_dict = new_data
+    # print(len(filtered_dict['112E']))
+
+
+
+    #sampled_list = random.sample(filtered_dict.keys(), 6)
+    sampled_list = ['5A0A','111F','D018','D713','120F','868C']
     subgroup_list = Util.generate_subgroups(subgroup_size, arr=sampled_list)
     all_pos = {}
-    return filtered_dict, subgroup_list
-    cl = all_pos.keys()
-
-
+    return filtered_dict, subgroup_list,sampled_list
+    #cl = all_pos.keys()
 root = tk.Tk()
-root.title("WiFi path 4 of 5")
-
+root.attributes("-fullscreen", True)
+root.title("WiFi path 6 of 4")
 # create a figure for the plot
-fig = Figure(figsize=(5, 4), dpi=100)
+fig = Figure(figsize=(6, 4), dpi=100)
 
 # create a canvas to display the plot
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
+#canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 # create a frame for the buttons
 controls_frame = tk.Frame(root)
 controls_frame.pack(side=tk.BOTTOM)
 
-fig.clf()
-ax = fig.add_subplot(111, projection='3d') 
-ax.set_xlim([0, 10])
-ax.set_ylim([0, 10])
-ax.set_zlim([0, 10])
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-real_person_path = Util.interpolate_points(Measurement.points_exp, 20)
-
-x = [point["x"] for point in real_person_path]
-y = [point["y"] for point in real_person_path]
-z = [point["z"] for point in real_person_path]
-ax.scatter(x, y, z, c="r", marker="o")
 def plot():
+
+    fig.clf()
+    ax = fig.add_subplot(111) 
+    ax.set_xlim([-1, 10])
+    ax.set_ylim([-5, 10])
+   # ax.set_zlim([0, 10])
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    #ax.set_zlabel("Z")
+    real_person_path = Util.interpolate_points(Measurement.points_exp, 20)
+
+    x = [point["x"] for point in real_person_path]
+    y = [point["y"] for point in real_person_path]
+    #z = [point["z"] for point in real_person_path]
+    #ax.scatter(x, y, z, c="r", marker="o")
+    ax.scatter(x, y, c="r", marker="o")
     global Colors
     global Subgroup_list 
     global Filtered_dict
     global Index
+    global Timestamp_list
+    global Points_list
+    global Sampled_list
     all_pos = {}
-
+    all_gt=[]
     for i, subgroup in enumerate(Subgroup_list):
         measurements = []
         for ap in subgroup:
             try:
-                measurement = Filtered_dict[ap].pop(0)
+                measurement = Filtered_dict[ap][0]
                 measurement.distance = bias(measurement.distance)
+                measurement.ground_truth = (
+                                           Util.interpolate_from_timestamp_to_location(
+                                                Points_list,
+                                                Timestamp_list,
+                                                measurement.timestamp,
+                                            )
+                                        )
                 measurements.append(measurement)
             except:
                 print("e")
         position = gradient_descent.train(measurements, {"x": 0, "y": 0, "z": 0})
+        min_timestamp = min(m.timestamp for m in measurements)
+        max_timestamp = max(m.timestamp for m in measurements)
+        time_diff_milliseconds = max_timestamp - min_timestamp
+        time_diff_seconds = time_diff_milliseconds / 1000
+
+        print(f"Time difference in milliseconds: {time_diff_seconds}")
+#        if time_diff_seconds <=1:
+        all_gt.append(measurements[0].ground_truth)
         if subgroup in all_pos:
             all_pos[subgroup].append(position)
         else:
             all_pos[subgroup] = [position]
+        for ap in Sampled_list: 
+            Filtered_dict[ap].pop(0)
+    column=10    
     for i,key in enumerate(all_pos):
         point = all_pos[key][0]
-        ax.scatter(point['x'], point['y'],point['z'], s=20, c=Colors[key], marker="x")
+        text = f"{key}"
+        y_random = column  # generate a random y value between 6 and 10
+        x_random = (i%4)*2   # generate a random x value between 0 and 10
+        if(i%4 !=0):
+            x_random = (i%4)*2   # generate a random x value between 0 and 10
+        else:
+            column -=1
+        ax.scatter(x_random, y_random)
+        ax.plot([x_random, point['x']], [y_random, point['y']],linestyle=":",alpha=0.4)
+
+        text_x = x_random + 0.2  # add a small offset to the x coordinate
+        text_y = y_random
+        ax.text(text_x, text_y, text,fontsize=8)
+        ax.scatter(point['x'], point['y'], s=20, c=Colors[key], marker="x")
+
+    for gt in all_gt:
+        ax.scatter(gt['x'], gt['y'], s=20, c='b', marker="o")
+        #ax.scatter(point['x'], point['y'],point['z'], s=20, c=Colors[key], marker="x")
     Index+=1    
     canvas.draw()
+    screenshot = subprocess.check_output(["scrot", "-u", "-"], stderr=subprocess.DEVNULL)
 
+    # save the cropped screenshot as an image file
+    filename = "plots/plot_{}.png".format(Index)  # include the counter variable in the filename
+    fig.savefig(filename)
 
 button = tk.Button(controls_frame, text="Plot", command=plot)
 button.pack(side=tk.LEFT)
 
 # create an input text box
 entry_exp = tk.Entry(controls_frame)
-entry_exp.insert(0, "EXP_74")
+entry_exp.insert(0, "EXP_56")
 entry_exp.pack(side=tk.LEFT)
 # start the main event loop
-Filtered_dict, Subgroup_list = process(4, entry_exp.get())
+Timestamp_list = Util.read_timestamps(
+            f"../CHECKPOINTS/CHECKPOINT_EXP_73"
+        )
+Filtered_dict, Subgroup_list,Sampled_list = process(4, entry_exp.get())
 Colors = None
 Colors = Util.generate_color_dict_v1(set(Subgroup_list))
 Index = 0
+Points_list = Util.generate_intermediate_points(Measurement.points_exp)
 
 root.mainloop()
--- test.py
+++ test.py
@@ -1,145 +1,36 @@
-from sklearn.cluster import DBSCAN
-import numpy as np
-import threading
-import math
-import os
-import sys
+
 import tkinter as tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
-cwd = os.getcwd()
-sys.path.insert(0, os.path.join(cwd, "classes"))
-sys.path.insert(0, os.path.join(cwd, "commons"))
-from APLocation import APLocation
-from GradientDescent import GradientDescent
-from GradientDescentFixedZ import GradientDescentFixedZ
-from Measurement import Measurement
-from Synthetic import Synthetic
-from Util import (
-    generate_intermediate_points,
-    generate_person_path,
-    generate_subgroups,
-    group_measurements_by_bssid,
-    interpolate_from_timestamp_to_location,
-    interpolate_points,
-    read_timestamps,
-    calculate_distance,
-    append_to_file,
-    generate_color_dict,
-)
-def process_data(bias_func):
-    measurements = Measurement.read_json_file("./new_file.json", "EXP_42", "802.11mc")
-    measurements_dict = group_measurements_by_bssid(measurements)
-    gradient_descent = GradientDescent(
-        learning_rate=0.01, max_iterations=1000, tolerance=1e-5
-    )
-    print((measurements_dict.keys()))
-    subgroup_list = generate_subgroups(4, arr=list(measurements_dict.keys()))
-    all_pos = []
-    ground_truth = None
-    for i, subgroup in enumerate(subgroup_list):
-        measurements = []
-        for ap in subgroup: 
-                measurement = measurements_dict[ap][1]
-                measurement.distance = bias_func(measurement.distance)
-                ground_truth = measurement.ground_truth
-                measurements.append(measurement)
-        position = gradient_descent.train(measurements, {"x": 0, "y": 0, "z": 0})
-        all_pos.append(position)
+import matplotlib.pyplot as plt
 
-    l = [[element["x"], element["y"], element["z"]] for element in all_pos]
-    X = np.array(l)
-
-# instantiate DBSCAN object with epsilon=1.0 and min_samples=2
-    dbscan = DBSCAN(eps=0.30, min_samples=2)
-
-# fit the DBSCAN model to the data
-    dbscan.fit(X)
-
-# print the cluster labels (-1 indicates noise)
-    arr_label = dbscan.labels_
-    color_dict = generate_color_dict(set(arr_label))
-    return all_pos,color_dict,arr_label
-
-# create the main window
+# create a tkinter window
 root = tk.Tk()
-root.title("Line Plotter")
+root.geometry("500x500")
 
-# create a figure for the plot
-fig = Figure(figsize=(5, 4), dpi=100)
+# create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
-# create a canvas to display the plot
+# add some data to the plot
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+z = [1, 2, 3, 4, 5]
+ax.scatter(x, y, z)
+
+# set the limits of the x, y, and z axes
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_zlim(0, 10)
+
+# create a canvas for the plot and add it to the tkinter window
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+canvas.get_tk_widget().pack()
 
-# create a frame for the sliders, button, and dropdown
-controls_frame = tk.Frame(root)
-controls_frame.pack(side=tk.BOTTOM)
-
-# create a label for the slope slider
-slope_label = tk.Label(controls_frame, text="Slope (m):")
-slope_label.pack(side=tk.LEFT)
-
-# create a slider for the slope
-slope_slider = tk.Scale(controls_frame, from_=-5, to=5, resolution=0.1, orient=tk.HORIZONTAL)
-slope_slider.pack(side=tk.LEFT)
-
-# create a label for the intercept slider
-intercept_label = tk.Label(controls_frame, text="Intercept (b):")
-intercept_label.pack(side=tk.LEFT)
-
-# create a slider for the intercept
-intercept_slider = tk.Scale(controls_frame, from_=-10, to=10, resolution=0.1, orient=tk.HORIZONTAL)
-intercept_slider.pack(side=tk.LEFT)
-
-# create a dropdown to select 2D or 3D plot type
-plot_type_var = tk.StringVar(value="2D")
-plot_type_dropdown = tk.OptionMenu(controls_frame, plot_type_var, "2D", "3D")
-plot_type_dropdown.pack(side=tk.LEFT)
-
-# create a function to plot the line
-def plot_line():
-    # clear the previous plot
-    fig.clf()
-
-    # get the slope and intercept values from the sliders
-    m = slope_slider.get()
-    b = intercept_slider.get()
-    if(m==0):
-        m=0.1
-    bias_func = lambda x: x//(m) - b
-    # get the selected plot type from the dropdown
-    plot_type = plot_type_var.get()
-
-    if plot_type == "2D":
-        # create a 2D plot with the line equation y = m*x + b
-        ax = fig.add_subplot(111)
-        x = [-10, 10]
-        y = [m*x_i + b for x_i in x]
-        ax.plot(x, y)
-    elif plot_type == "3D":
-        all_pos,color_dict,arr_label = process_data(bias_func)
-        ax = fig.add_subplot(111, projection='3d')
-        for i,d in enumerate(all_pos):
-            x, y, z = d["x"], d["y"], d["z"]
-            ax.scatter(x, y, z,c=color_dict[arr_label[i]])
-        ax.scatter(8.5, 0.5, 0,c='black',marker='x')
-        ax.set_xlim([0, 10])
-        ax.set_ylim([0, 10])
-        ax.set_zlim([0, 10])
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_zlabel("Z")
-
-    # update the canvas with the new plot
-    canvas.draw()
-
-# create a button to plot the line
-plot_button = tk.Button(controls_frame, text="Plot", command=plot_line)
-plot_button.pack(side=tk.LEFT)
-
-# start the main event loop
+# start the tkinter event loop
 root.mainloop()
+
 
